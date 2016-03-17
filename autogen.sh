b57e11218214ca3018469337e6cbd9461b1c288b
Mathias Rav <rav@cs.au.dk>
2016-03-17 12:25:02 +0100

    Remove faulty bison/flex checks

--- autogen.sh
+++ autogen.sh
@@ -4,37 +4,6 @@
 # terms of the General Public License version 3 (or later).
 # See COPYING file for license details.
 
-bisonver=`bison --version`
-
-if [ "$bisonver" = "" ]; then
-	echo Bison is needed to compile Claws Mail git
-	exit 1
-fi
-
-if [ "$LEX" != "" ]; then
-	flexver=`$LEX --version|sed "s/.* //"`
-else
-	flexver=`flex --version|sed "s/.* //"`
-fi
-
-if [ "$flexver" = "" ]; then
-	echo Flex 2.5.31 or greater is needed to compile Claws Mail git
-	exit 1
-else
-	flex_major=`echo $flexver|sed "s/\..*//"`
-	flex_minor=`echo $flexver|sed "s/$flex_major\.\(.*\)\..*/\1/"`
-	flex_micro=`echo $flexver|sed "s/$flex_major\.$flex_minor\.\(.*\)/\1/"`
-
-	flex_numversion=$(expr \
-		$flex_major \* 10000 + \
-		$flex_minor \* 100 + \
-		$flex_micro)
-
-	if [ $flex_numversion -lt 20531 ]; then
-		echo Flex 2.5.31 or greater is needed to compile Claws Mail git
-		exit 1
-	fi
-fi
 
 aclocal -I m4 \
   && libtoolize --force --copy \
