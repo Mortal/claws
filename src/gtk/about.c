5b48167b177336f033b9e90fc73c1ae4f01e5107
Mathias Rav <rav@cs.au.dk>
2016-07-31 12:34:22 +0200

    Add time when Claws was compiled

--- src/gtk/about.c
+++ src/gtk/about.c
@@ -819,7 +819,7 @@ static void about_create(void)
 	gtk_label_set_justify(GTK_LABEL(label), GTK_JUSTIFY_CENTER);
 	gtk_box_pack_start(GTK_BOX(vbox2), label, FALSE, FALSE, 0);
 	markup = g_markup_printf_escaped
-		("<span weight=\"bold\" size=\"xx-large\">Claws Mail</span>\nversion %s",
+		("<span weight=\"bold\" size=\"xx-large\">Claws Mail</span>\nversion %s\ncompiled " __TIMESTAMP__,
 		 VERSION_GIT_FULL);
 	gtk_label_set_markup(GTK_LABEL(label), markup);
 	g_free(markup);
